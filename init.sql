e7582e07b87d782917aaef278e2a6895d4884a60
Raoni Medinilha <53878943+rallz-dev@users.noreply.github.com>
2024-05-17 00:27:24 -0300

    Add files via upload

---
+++ init.sql
@@ -0,0 +1,17 @@
+\c postgres;
+
+create table if not exists contacts (
+  id_contact int generated always as identity,
+  contact_name text,
+  contact_email text,
+  contact_phone_number text,
+  contact_cpf text
+);
+
+create table if not exists users (
+  id_contact int generated always as identity,
+  user_name text,
+  user_email text,
+  user_password text
+);
+
