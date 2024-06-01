c01c503d684485ff72799946ae7a0735ee999701
edu <bfwaend@gmail.com>
2024-06-01 14:52:12 -0300

    Add some style to NewContact form

--- ex01/web-ui/src/pages/new-contact/NewContact.tsx
+++ ex01/web-ui/src/pages/new-contact/NewContact.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { NewContactData } from "../../interfaces/ContactCardProps";
 import axios from "axios";
+import './NewContact.css'
 
 const NewContact = () => {
   const [newContactData, setNewContactData] = useState<NewContactData>({
@@ -17,7 +18,7 @@ const NewContact = () => {
       Object.values(newContactData).filter(value => value === "").length > 0
     ) {
       setIsShowingError(true);
-      return ;
+      return;
     }
     axios.post('http://localhost:8080/v1/create-contact',
       { contact: newContactData }
@@ -28,39 +29,47 @@ const NewContact = () => {
   return (
     <>
       <h1 style={{ margin: "0" }}>New Contact</h1>
-      <form onSubmit={handleSubmit}>
-        <label>Name:</label>
-        <input
-          type="text"
-          value={newContactData.name}
-          onChange={(e) =>
-            setNewContactData({ ...newContactData, name: e.target.value })
-          }
-        />
-        <label>Email:</label>
-        <input
-          type="text"
-          value={newContactData.email}
-          onChange={(e) =>
-            setNewContactData({ ...newContactData, email: e.target.value })
-          }
-        />
-        <label>Phone:</label>
-        <input
-          type="text"
-          value={newContactData.phone}
-          onChange={(e) =>
-            setNewContactData({ ...newContactData, phone: e.target.value })
-          }
-        />
-        <label>CPF:</label>
-        <input
-          type="text"
-          value={newContactData.cpf}
-          onChange={(e) =>
-            setNewContactData({ ...newContactData, cpf: e.target.value })
-          }
-        />
+      <form onSubmit={handleSubmit} className="new-contact-form">
+        <div className="form-item">
+          <label>Name:</label>
+          <input
+            type="text"
+            value={newContactData.name}
+            onChange={(e) =>
+              setNewContactData({ ...newContactData, name: e.target.value })
+            }
+          />
+        </div>
+        <div className="form-item">
+          <label>Email:</label>
+          <input
+            type="text"
+            value={newContactData.email}
+            onChange={(e) =>
+              setNewContactData({ ...newContactData, email: e.target.value })
+            }
+          />
+        </div>
+        <div className="form-item">
+          <label>Phone:</label>
+          <input
+            type="text"
+            value={newContactData.phone}
+            onChange={(e) =>
+              setNewContactData({ ...newContactData, phone: e.target.value })
+            }
+          />
+        </div>
+        <div className="form-item">
+          <label>CPF:</label>
+          <input
+            type="text"
+            value={newContactData.cpf}
+            onChange={(e) =>
+              setNewContactData({ ...newContactData, cpf: e.target.value })
+            }
+          />
+        </div>
         <button type="submit">Submit</button>
       </form>
       {isShowingError && <p> Can't send empty values! </p>}
